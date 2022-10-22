18feffe2984030d443e86a7af8c5d2f34de3d386
chanchal16 <rajputchanchal17@gmail.com>
2022-10-22 16:43:01 +0530

    Create Navbar.jsx

---
+++ src/components/common/Navbar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Navbar = () => {
+  return (
+    <nav className='bg-dark-gray p-3'>
+        <span className='text-white text-3xl font-display'>Memo</span>
+    </nav>
+  )
+}
