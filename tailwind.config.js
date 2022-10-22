e7aca6ffc58c7558de401c5f4b9902443b0d2053
chanchal16 <rajputchanchal17@gmail.com>
2022-10-22 16:43:36 +0530

    Update - add theme colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,14 @@
 module.exports = {
   purge: ['./public/index.html','./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    colors:{
+      'gray':'#94a3b8',
+      'dark-gray':'#64748b',
+      'white': '#ffffff',
+    },
+    fontFamily:{
+      'display':['Akaya Kanadaka', 'cursive']
+    },
     extend: {},
   },
   plugins: [],
