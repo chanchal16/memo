1fb2e7247c0f3763fe576f0f2313b1e8e7c741f1
chanchal16 <rajputchanchal17@gmail.com>
2022-10-23 14:23:48 +0530

    Update - add sidebar component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import "./tailwind.css";
 import "./main.css";
+import { Navbar } from "./components/common/Navbar";
+import { Sidebar } from "./components/common/Sidebar";
+import { Footer } from "./components/common/Footer";
+import { Modal } from "./components/Modal";
 
 function App() {
   return (
     <div className="App">
-      <h1 className="text-5xl text-red-600">memo</h1>
+      <Navbar/>
+      
+      <main>
+      <Sidebar/>
+        <h5 className="text-4xl ml-36">iuio hgjh</h5>
+        <Modal/>
+      </main>
+      {/* <Footer/>  */}
     </div>
   );
 }
