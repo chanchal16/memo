f99d3868b4ea41e4f87fbfa5fa11e20690660c47
chanchal16 <rajputchanchal17@gmail.com>
2022-10-23 14:24:15 +0530

    Update - add nav link

--- src/components/common/Sidebar.jsx
+++ src/components/common/Sidebar.jsx
@@ -3,23 +3,27 @@ import {AiOutlineBulb} from 'react-icons/ai';
 import {RiInboxArchiveLine} from 'react-icons/ri';
 import {IoTrashOutline} from 'react-icons/io5';
 import {MdLabelOutline} from 'react-icons/md'
+import { NavLink } from "react-router-dom";
 
 export const Sidebar = () => {
+  // const getClassName = ({isActive})=>{
+  //   return isActive ? "link active" : "link"        
+  // }
   return (
     <aside className='flex  gap-4 py-4 sidebar flex-col'>      
         <ul className='flex  gap-6 flex-col'>
-            <li className='flex items-center gap-2 p-2 link active'>
+            <NavLink to={'/'} className={`flex items-center gap-2 p-2 link active`}>
                 <AiOutlineBulb size={'1.5em'} />
                <span className='text-xl'>Notes</span> 
-            </li>
-            <li className='flex items-center gap-2 p-2 link'>
+            </NavLink>
+            <NavLink to={'/archive'} className={`flex items-center gap-2 p-2 link`}>
                 <RiInboxArchiveLine size={'1.5em'} />
                 <span className='text-xl'>Archive</span>
-            </li>
-            <li className='flex items-center gap-2 p-2 link'>
+            </NavLink>
+            <NavLink to={'/trash'} className='flex items-center gap-2 p-2 link'>
                 <IoTrashOutline size={'1.5em'} />
                <span className='text-xl'>Trash</span> 
-            </li>
+            </NavLink>
         </ul>
         <span className='text-xl'>Labels</span>
         
